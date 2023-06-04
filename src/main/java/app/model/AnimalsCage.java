6ed5ac5abf33b0d1dbd037a443863bd202d6fc2c
hop1999 <cosmik9@mail.ru>
2023-06-04 15:49:26 +0300

    Update AnimalsCage.java

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -4,12 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-// не реализован 4 пункт задания “4) На основе класса Timer создайте бин. Свяжите с AnimalCage.
-// Проверьте, что при выполнении метода main время, которое пишет таймер, одно и тоже.”
-//
-//код-стайл - между полем и методом добавь пустую строку.
-// Время одинаковое, бин создал. Или ты меня с кем то перепутал, или я очень сильно плыву :( (кодстайл поправил)
-
 @Component
 public class AnimalsCage {
 
@@ -24,7 +18,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
